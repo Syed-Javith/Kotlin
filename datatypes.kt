471a4995926240fd5cf66c06a5d64ee99c1224af
Syed-Javith <syedjavith14@gmail.com>
2024-01-23 18:38:49 +0530

    Initialized

---
+++ datatypes.kt
@@ -0,0 +1,16 @@
+fun main(){
+    // main data types are : Numeric , Boolean , String , Char , Arrays
+
+    // In Numeric , we have Int , Double , Short , Long , Byte
+
+    var numInt : Int = 2
+    var numDouble : Double = 3.14
+    println(numInt)
+    println(numDouble)
+
+    var ch : Char = 'd'
+    println(ch)
+
+    var str : String = "Hello World"
+    println(str)
+}
---
+++ declaration.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var name : String = "Hello"
+    println(name)
+    // var can be changed but data type cannot 
+    name = "World"
+    println(name)
+    // name = 3 // error
+   // print(name)
+
+   // val is also used to declare values but neither values nor data type can be changed
+    val pi = 3.143
+    println(pi)
+    // pi = 3 is error
+}
